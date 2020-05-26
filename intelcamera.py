c8d6007943f00994baa7dcdf95aa13f1b8a10deb
Jamie Tremaine <jamie@iasmachine.com>
2020-05-26 01:16:34 -0400

    Added color picture taking functionality for multiple cameras

--- intelcamera.py
+++ intelcamera.py
@@ -1,51 +1,76 @@
 import os
 import sys
-import cv2
-import pyrealsense2 as rs
 import numpy as np
 import uuid
+from functools import reduce
 
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+import cv2
+import pyrealsense2 as rs
+from realsense_device_manager import DeviceManager
 
-pipeline.start(config)
+CAMERA_RESOLUTION_WIDTH = 1920#1280
+CAMERA_RESOLUTION_HEIGHT = 1080#720
+CAMERA_FRAME_RATE = 30
+
+DATA_FOLDER_NAME = 'data'
+
+CV_WINDOW_NAME = 'RealSense'
+INIT_WINDOW_WIDTH = 700
+INIT_WINDOW_HEIGHT = 700
 
 def captureFrame(img):
-    print("Capturing Image...")
-    data_dir = os.path.join(os.getcwd(), 'data')
+    data_dir = os.path.join(os.getcwd(), DATA_FOLDER_NAME)
     if (not os.path.isdir(data_dir)):
         os.mkdir(data_dir)
 
     img_name = str(uuid.uuid4()) + ".jpg"
     img_dir = os.path.join(data_dir, img_name)
 
-    cv2.imwrite(img_dir, img)
-    print("Captured Image " + img_name)
+    return cv2.imwrite(img_dir, img), img_dir
 
-try:
-    while True:
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        if not color_frame:
-            continue
-<<<<<<< HEAD
+def run():
+    rs_config = rs.config()
+    rs_config.enable_stream(rs.stream.color, CAMERA_RESOLUTION_WIDTH, CAMERA_RESOLUTION_HEIGHT, rs.format.bgr8, CAMERA_FRAME_RATE)
 
-        color_image = np.asanyarray(color_frame.get_data())
-        cv2.imshow('RealSense', color_image)
-=======
-        
-        color_image = np.asanyarray(color_frame.get_data())
-        cv2.imshow('RealSense', color_image)
-        
->>>>>>> 6501d6d2c29f046b37c878b06e778f19393cc817
-        key = cv2.waitKey(1)
-        if (key == 112): # 'p' key
-            captureFrame(color_image)
-        elif (key == 27): # 'esc' key
-            break
-finally:
-    pipeline.stop()
+	# Use the device manager class to enable the devices and get the frames
+    device_manager = DeviceManager(rs.context(), rs_config)
+    device_manager.enable_all_devices()
 
-print("Finished")
+    assert (len(device_manager._available_devices) > 0)
+    
+    try:
+        while True:
+            frames = device_manager.poll_frames()
+
+            color_images = []
+            for key in frames.keys():
+                color_frame = frames[key][rs.stream.color].get_data()
+                color_image = np.asanyarray(color_frame)
+                color_images.append(color_image)
+
+            if len(color_images) == 0:
+                continue
+
+            images = np.vstack(color_images)
+
+            cv2.namedWindow(CV_WINDOW_NAME, cv2.WINDOW_NORMAL)
+            cv2.resizeWindow(CV_WINDOW_NAME, INIT_WINDOW_WIDTH, INIT_WINDOW_HEIGHT)
+            cv2.imshow(CV_WINDOW_NAME, images)
+
+            k = cv2.waitKey(1)
+            if k == 112: # 'p' key
+                print("Capturing images...")
+                for color_image in color_images:
+                    status, dir = captureFrame(color_image)
+                    if status: print("Saved image to " + dir)
+                    else: print("A problem occurred when capturing the frame.")
+            elif k == 27:
+                break
+    except KeyboardInterrupt:
+        print("The program was interupted by the user. Closing the program...")
+    finally:
+        device_manager.disable_streams()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    run()
